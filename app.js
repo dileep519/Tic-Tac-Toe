ab367cf80a8bb3ed3cb0918d767d55366c362cfd
= <=>
2020-12-13 11:16:24 +0530

    Tic Tac Toe assignment

---
+++ app.js
@@ -0,0 +1,85 @@
+let count=0;
+let win=false;
+
+
+    
+    document.getElementById('One').addEventListener('click',()=>{
+        if(count%2==0){
+            document.getElementById('One').textContent='O';
+        }else{
+            document.getElementById('One').textContent='X';
+        }
+        count++;
+    })
+     
+    document.getElementById('Two').addEventListener('click',()=>{
+        if(count%2==0){
+            document.getElementById('Two').textContent='O';
+        }else{
+            document.getElementById('Two').textContent='X';
+        }
+        count++;
+    })
+    
+    document.getElementById('Three').addEventListener('click',()=>{
+        if(count%2==0){
+            document.getElementById('Three').textContent='O';
+        }else{
+            document.getElementById('Three').textContent='X';
+        }
+        count++;
+    })
+    
+    document.getElementById('Four').addEventListener('click',()=>{
+        if(count%2==0){
+            document.getElementById('Four').textContent='O';
+        }else{
+            document.getElementById('Four').textContent='X';
+        }
+        count++;
+    })
+    
+    document.getElementById('Five').addEventListener('click',()=>{
+        if(count%2==0){
+            document.getElementById('Five').textContent='O';
+        }else{
+            document.getElementById('Five').textContent='X';
+        }
+        count++;
+    })
+    
+    document.getElementById('Six').addEventListener('click',()=>{
+        if(count%2==0){
+            document.getElementById('Six').textContent='O';
+        }else{
+            document.getElementById('Six').textContent='X';
+        }
+        count++;
+    })
+    
+    document.getElementById('Seven').addEventListener('click',()=>{
+        if(count%2==0){
+            document.getElementById('Seven').textContent='O';
+        }else{
+            document.getElementById('Seven').textContent='X';
+        }
+        count++;
+    })
+    
+    document.getElementById('Eight').addEventListener('click',()=>{
+        if(count%2==0){
+            document.getElementById('Eight').textContent='O';
+        }else{
+            document.getElementById('Eight').textContent='X';
+        }
+        count++;
+    })
+    
+    document.getElementById('Nine').addEventListener('click',()=>{
+        if(count%2==0){
+            document.getElementById('Nine').textContent='O';
+        }else{
+            document.getElementById('Nine').textContent='X';
+        }
+        count++;
+    })
